a9f12f747a4d42672c233fe20d371726faf32a1d
Medmana <ahiffonman@gmail.com>
2025-04-11 05:15:58 +0100

    bi

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,19 +1,14 @@
-import AboutSectionOne from "@/components/About/AboutSectionOne";
-import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+
 import Blog from "@/components/Examen";
-import Brands from "@/components/Brands";
+
 import ScrollUp from "@/components/Common/ScrollUp";
-import Contact from "@/components/Contact";
-import Features from "@/components/Features";
-import Hero from "@/components/Hero";
-import Pricing from "@/components/Pricing";
+
 import Patients from "@/components/Patients";
-import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Free Next.js Template for Startup and SaaS",
-  description: "This is Home for Startup Nextjs Template",
+  title: "AI4CKD",
+  description: "AI4CKD - AI for Chronic Kidney Disease",
   // other metadata
 };
 
